00b1be6d4f22ae239bdf6198ae8e8c8cdc2110a2
Vega <vega@localhost.localdomain>
2021-05-17 15:05:43 +0200

    Automated tests

--- Tests/testScript.sh
+++ Tests/testScript.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 CompiledPgm="../src/minicc"
-nbTestsPassed=0
-nbTests=0
 #pour chaque fichier de test
-for D in `find .  -mindepth 1 -type d  `;do
-    echo "Tests du dossier" $D
-    #Enleve les dossiers intermédiaires
-    if [ ! -d $D/OK ] 
-    then
+for D in `find .  -mindepth 2 -type d  `;do
+    if [[ "$D" =~ OK ]];then
         for f in $D/*.tst; do
             value+=$(cat "$f")
+            echo tested command : $CompiledPgm $value
             "$CompiledPgm" "$value"
             if [ $? -eq 0 ]
             then
                 echo "$f" passed
-                nbTestsPassed+=1
             else
                 echo "$f" not passed
             fi
-            nbTests+=1
-        done
+        done   
+    elif [[ "$D" =~ KO ]];then
+        for f in $D/*.tst; do
+            value+=$(cat "$f")
+            echo tested command : $CompiledPgm $value
+            "$CompiledPgm" "$value"
+            if [ $? -eq 0 ];then
+                echo "$f" passed but should not                
+            else
+                echo "$f" failed as expected
+            fi
+        done   
     fi
 done
