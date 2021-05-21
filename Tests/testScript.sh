fa864e8936fc321e508c323f85b1671775b6a186
Vega <vega@localhost.localdomain>
2021-05-21 16:09:02 +0200

    Tests

--- Tests/testScript.sh
+++ Tests/testScript.sh
@@ -2,23 +2,31 @@
 CompiledPgm="../src/minicc"
 #pour chaque fichier de test
 for D in `find .  -mindepth 2 -type d  `;do
+    tmpPgm=$CompiledPgm
+    if [[ "$D" =~ Verif ]];then
+    tmpPgm+=" -v"
+    elif [[ "$D" =~ Syntaxe ]];then
+    tmpPgm+=" -s"
+    fi
+    echo $tmpPgm
     if [[ "$D" =~ OK ]];then
         for f in $D/*.tst; do
-            value+=$(cat "$f")
-            echo tested command : $CompiledPgm $value
-            "$CompiledPgm" "$value"
+            value=$(cat "$f")
+            echo tested program : $tmpPgm 
+            if[ [ "$D" =~ OK ]]
+            "$tmpPgm" $f
             if [ $? -eq 0 ]
             then
                 echo "$f" passed
             else
                 echo "$f" not passed
             fi
-        done   
+        done  
     elif [[ "$D" =~ KO ]];then
         for f in $D/*.tst; do
-            value+=$(cat "$f")
-            echo tested command : $CompiledPgm $value
-            "$CompiledPgm" "$value"
+            value=$(cat "$f")
+            echo tested  : $tmpPgm $value
+            "$tmpPgm" $f
             if [ $? -eq 0 ];then
                 echo "$f" passed but should not                
             else
