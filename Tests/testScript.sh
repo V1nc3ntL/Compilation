e572da154fe4d6bb14dd8f8323db4a191fc9bd78
Vega <vega@localhost.localdomain>
2021-05-21 17:17:45 +0200

    Verbose and Gencode

--- Tests/testScript.sh
+++ Tests/testScript.sh
@@ -1,32 +1,38 @@
 #!/bin/bash
-CompiledPgm="../src/minicc"
+CompiledPgmName="../src/minicc"
 #pour chaque fichier de test
+
+if [ ! -f "minicc" ];then 
+
+    cp $CompiledPgmName .
+
+fi
+
+pgm="./minicc"
+
 for D in `find .  -mindepth 2 -type d  `;do
-    tmpPgm=$CompiledPgm
+
     if [[ "$D" =~ Verif ]];then
-    tmpPgm+=" -v"
+        opt="-v"
     elif [[ "$D" =~ Syntaxe ]];then
-    tmpPgm+=" -s"
+        opt="-s"
+    else
+        opt=""
     fi
-    echo $tmpPgm
+
     if [[ "$D" =~ OK ]];then
-        for f in $D/*.tst; do
-            value=$(cat "$f")
-            echo tested program : $tmpPgm 
-            if[ [ "$D" =~ OK ]]
-            "$tmpPgm" $f
-            if [ $? -eq 0 ]
-            then
+        for f in $D/*.tst; do        
+            $pgm $opt $f
+            if [ $? -eq 0 ];then
                 echo "$f" passed
             else
                 echo "$f" not passed
             fi
         done  
+        
     elif [[ "$D" =~ KO ]];then
         for f in $D/*.tst; do
-            value=$(cat "$f")
-            echo tested  : $tmpPgm $value
-            "$tmpPgm" $f
+            $pgm $opt $f
             if [ $? -eq 0 ];then
                 echo "$f" passed but should not                
             else
