779372718796e0fab64084981470c9903b9d0101
Vincent <vega@localhost.localdomain>
2021-06-01 15:43:46 +0200

    remove passe2 error

--- src/passe_2.c
+++ src/passe_2.c
@@ -5,10 +5,11 @@
 #include "miniccutils.h"
 #include "instBuffer.h"
 #include <string.h>
-#define ALIGN(a) ( a % 4 ? a - a % 4 + 4 : a)
+
 
 int wreckOffset=0;
 int varDecl = 0;
+
 int getStartOffset(node_t root){
     if (root->nops > 1)
 	{
@@ -39,11 +40,10 @@ void plus_inst(node_t root){
                 a.tre = create_inst_addiu;
                 addToInstBuffer(a,r1,0,tmp);                        
             }else{
-
-                /*
+        
                 a.uno = push_temporary;
                 addToInstBuffer(a,r1,0,0);
-                release_reg();*/
+                release_reg();
             }
 
         }else{
@@ -89,9 +89,7 @@ void plus_inst(node_t root){
                 addToInstBuffer(a,r1,0,0);
                 a.tre = create_inst_addiu;
                 addToInstBuffer(a,r1,0,tmp);
-                
             }
-              
         }else{
                 r_passe_2(root->opr[1]);
                 
@@ -169,12 +167,12 @@ void r_passe_2_print(node_t root){
             case NODE_IDENT :
                     // Récupérer la valeur dans un registre
                     r1 = get_current_reg();
+                    a.duo = create_inst_lui;
+                    addToInstBuffer(a,r1, 0x1001,0);   
+ 
                     a.tre = create_inst_lw;
-                    if(root->type == TYPE_INT)
-                        addToInstBuffer(a,4,root->decl_node->offset-4,r1);
-                    else{
-                        addToInstBuffer(a,4,root->decl_node->offset,r1);
-                    }
+                    addToInstBuffer(a,4,root->offset-4,r1);
+               
                     a.tre = create_inst_addiu;
                     addToInstBuffer(a,2,0,1);
                     // Mettre la valeur a afficher dans le syscall
@@ -236,39 +234,32 @@ void r_passe_2(node_t root){
             case NODE_DECL:
 
                 varDecl++;
-                // Si la variable est une déclaration initialisée
-                if( root->opr[1]->nature == NODE_INTVAL &&
-                    root->opr[0]->nature == NODE_IDENT){
-                    create_inst_word(root->opr[0]->ident,root->opr[1]->value);
-                }
-                else{
-                    create_inst_word(root->opr[0]->ident,root->opr[1]->value);
-                    
-                    if( root->opr[1]->nature == NODE_PLUS &&
-                        root->opr[0]->nature == NODE_IDENT  ){
-                        r_passe_2(root->opr[0]);
-                        r_passe_2(root->opr[1]);
-                        r1 = get_current_reg();
-                        if(reg_available()){
-                            allocate_reg();
-                            r2 = get_current_reg();
-                            a.duo = create_inst_lui;
-                            addToInstBuffer(a,r2, 0x1001,0);    
-                            a.tre = create_inst_sw;
-                            addToInstBuffer(a,r1,root->opr[0]->offset-4,r2);
-                        }else{
-                        }
+                if(root->opr[1]){
+                        // Si la variable est une déclaration initialisée
+                    if( root->opr[1]->nature == NODE_INTVAL &&
+                        root->opr[0]->nature == NODE_IDENT){
+                        create_inst_word(root->opr[0]->ident,root->opr[1]->value);
                     }
-                }  
-               /*  *//*
-                // Retrouver l'adresse de data
-               
-                    
-                    allocate_reg();
-                    
-                    
-                    release_reg();
-                }*/
+                    else{
+                        create_inst_word(root->opr[0]->ident,root->opr[1]->value);
+                        
+                        if( root->opr[1]->nature == NODE_PLUS &&
+                            root->opr[0]->nature == NODE_IDENT  ){
+                            r_passe_2(root->opr[0]);
+                            r_passe_2(root->opr[1]);
+                            r1 = get_current_reg();
+                            if(reg_available()){
+                                allocate_reg();
+                                r2 = get_current_reg();
+                                a.duo = create_inst_lui;
+                                addToInstBuffer(a,r2, 0x1001,0);    
+                                a.tre = create_inst_sw;
+                                addToInstBuffer(a,r1,root->opr[0]->offset,r2);
+                            }else{
+                            }
+                        }
+                    }  
+                }
                 break;
             case NODE_PLUS:
                 plus_inst(root);
@@ -300,6 +291,7 @@ void global_var(node_t root){
     if(root != NULL){
         switch(root->nature){
             case NODE_PROGRAM:
+            
                 if (root->nops > 1)
                 {
                     r_passe_2(root->opr[1]);
@@ -319,32 +311,27 @@ void global_var(node_t root){
                 create_inst_stack_deallocation(root->offset + get_temporary_max_offset());
                 break;
             case NODE_IDENT:
-               
                 break;
             case NODE_BLOCK:
                 r_passe_2(root->opr[0]);
                 r_passe_2(root->opr[1]);
                 break;
             case NODE_INTVAL:
-                
                 break;
             case NODE_AFFECT:
                 r_passe_2(root->opr[0]);
                 r_passe_2(root->opr[1]);
-                //create_inst_sw(r1, r2, r3);
                 break;
             case NODE_DECLS:
-                r_passe_2(root->opr[0]);
-                r_passe_2(root->opr[1]);
+                global_var(root->opr[0]);
+                global_var(root->opr[1]);
                 break;
             case NODE_DECL:
-        
-                r_passe_2(root->opr[0]);
-                r_passe_2(root->opr[1]);
-
-                if(!reg_available()){
-                    a.uno = push_temporary;
-                    addToInstBuffer(a,get_current_reg(),0,0);
+                if(!root->opr[1]){
+                    create_inst_word(root->ident,0);
+                }
+                else{
+                    create_inst_word(root->ident,root->opr[1]->value);
                 }
                 break;
             case NODE_PLUS:
@@ -354,10 +341,7 @@ void global_var(node_t root){
                 r_passe_2(root->opr[0]);
                 r_passe_2(root->opr[1]);
                 break;
-            
             default:
-             //   r_passe_2(root->opr[0]);
-               
                 break;
         }
     }
