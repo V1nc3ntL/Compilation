e2008449ddb509021f6ddcfd0a92226807bec9ab
Pe-Graves <pauledouardg@gmail.com>
2021-06-02 21:15:13 +0200

    pe_fin

--- src/passe_1.c
+++ src/passe_1.c
@@ -11,10 +11,17 @@ int trace_level;
 void analyse_node_ident (node_t n)
 {
   int32_t off;
+  // On regarde
   n -> decl_node = get_decl_node (n -> ident);
+
+  // S'il existe une première définition de la variable
+
   if (n -> decl_node)
-  {
+  { 
+    /// On met a jour l'offset
     n -> offset = n -> decl_node->offset;
+
+    //On met a jour le type
     n -> type   = n -> decl_node->type;
   }
 
@@ -29,7 +36,10 @@ void analyse_node_ident (node_t n)
 
     else
     {
+      // On met à jour l'offset
       n -> offset = n -> decl_node->offset;
+
+      //On met a jour le type
       n -> type   = n -> decl_node->type;
     }
   }
@@ -64,62 +74,77 @@ void analyse_node_global_ident (node_t n)
 
 void analyse_node_func (node_t n)
 {
-  //Faire analyse node
+  //On met a jour le type pour le NODE_IDENT
   n -> opr[1] -> type = n -> opr[0] -> type;
+
+  // On met a jour l'offset
   n -> offset = get_env_current_offset();
 }
 
 void analyse_node_stringval (node_t n)
 {
+  // On met a jour l'offset
   n -> offset = add_string (n -> str);
 }
 
 void analyse_node_program (node_t n)
 {
+  // On met a jour l'offset
   n -> offset = add_string (n -> str);
 }
 
 void analyse_global (node_t root)
 {
+  // Si la node est NULL
   if (!root)
   {
     return;
   }
 
+  //Switch en fonction de la nature de la node
   switch (root -> nature)
   {
     case NODE_PROGRAM:
-
+    // On push le context au tout début de l'arbre
     push_global_context ();
+    //Dans le cas, ou il des variables globales en plus
     if (root -> nops > 1)
     {
       analyse_global(root -> opr[0]);
       analyse_passe_1(root -> opr[1]);
     }
+    //  dans le cas où on a que le main
     else
     {
       analyse_passe_1(root -> opr[1]);
     }
+    // On pop le context apres la création de l'abre
     pop_context ();
     break;
 
     case NODE_IDENT:
+
+      // On effectue les opérations sur les champs de la NODE_IDENT
       analyse_node_global_ident(root);
       break;
 
     case NODE_LIST:
 
+      // on transmet le type aux fils de la NODE_LIST
       root -> opr[0] -> type  = root -> type;
       root -> opr[1] -> type  = root -> type;
+      //Appels récursifs
       analyse_passe_1 (root -> opr[0]);
       analyse_passe_1 (root -> opr[1]);
       break;
 
     case NODE_DECLS:
 
+      // On met a jour le champ decl_node pour les fils
       root -> opr[0] -> decl_node = root;
       root -> opr[1] -> decl_node = root;
       root -> type = root -> opr[0] -> type;
+      //Appels récursifs
       analyse_passe_1 (root -> opr[0]);
       analyse_passe_1 (root -> opr[1]);
       break;
@@ -128,63 +153,73 @@ void analyse_global (node_t root)
 
       if (root -> decl_node)
       {
-        if (root -> decl_node -> type != root -> opr[1] -> type || root->decl_node->type != root->opr[0]->type)
+        // En cas d'erreur
+        if (root -> decl_node -> type != root -> opr[1] -> type || root -> decl_node -> type != root -> opr[0] -> type)
         {
           exit (-1);
         }
         else
         {
-          root->opr[0]->type = root->decl_node->type;
-          root->opr[1]->type = root->decl_node->type;
+          //Met a jour les types pour les fils
+          root->opr[0] -> type = root -> decl_node -> type;
+          root->opr[1] -> type = root -> decl_node -> type;
         }
       }
       else
       {
-        root->opr[0]->type = root->opr[1]->type;
+        // On donne le type du second fils au premier fils 
+        root -> opr[0] -> type = root -> opr[1] -> type;
       }
-
-      analyse_passe_1 (root->opr[0]);
-      analyse_passe_1 (root->opr[1]);
+      //Appels récursifs
+      analyse_passe_1 (root -> opr[0]);
+      analyse_passe_1 (root -> opr[1]);
       break;
 
     case NODE_FUNC:
 
-      analyse_passe_1 (root->opr[0]);
-      analyse_passe_1 (root->opr[1]);
-      analyse_passe_1 (root->opr[2]);
+      //Appels récursifs
+      analyse_passe_1 (root -> opr[0]);
+      analyse_passe_1 (root -> opr[1]);
+      analyse_passe_1 (root -> opr[2]);
+      // Opérations sur la NODE_FUNC
       analyse_node_func (root);
       break;
 
     case NODE_BLOCK:
 
-      analyse_passe_1 (root->opr[0]);
-      analyse_passe_1 (root->opr[1]);
+      // Appels récursifs
+      analyse_passe_1 (root -> opr[0]);
+      analyse_passe_1 (root -> opr[1]);
       break;
 
     case NODE_FOR:
 
-      analyse_passe_1 (root->opr[0]);
-      analyse_passe_1 (root->opr[1]);
-      analyse_passe_1 (root->opr[2]);
-      analyse_passe_1 (root->opr[3]);
+      // Appels récursifs
+      analyse_passe_1 (root -> opr[0]);
+      analyse_passe_1 (root -> opr[1]);
+      analyse_passe_1 (root -> opr[2]);
+      analyse_passe_1 (root -> opr[3]);
       break;
 
     case NODE_LT:
 
-      analyse_passe_1 (root->opr[0]);
-      analyse_passe_1 (root->opr[1]);
+      // Appels récursifs
+      analyse_passe_1 (root -> opr[0]);
+      analyse_passe_1 (root -> opr[1]);
       break;
 
     case NODE_AFFECT:
 
-      analyse_passe_1 (root->opr[0]);
-      analyse_passe_1 (root->opr[1]);
+      // Appels récursifs
+      analyse_passe_1 (root -> opr[0]);
+      analyse_passe_1 (root -> opr[1]);
       break;
 
     case NODE_PLUS:
 
-      analyse_passe_1 (root->opr[0]);
-      analyse_passe_1 (root->opr[1]);
+      // Appels récursifs
+      analyse_passe_1 (root -> opr[0]);
+      analyse_passe_1 (root -> opr[1]);
       // On vérifie que le l'addition se fait sur deux
       // entités du même type
 
@@ -198,8 +233,10 @@ void analyse_global (node_t root)
 
     case NODE_MINUS:
 
-      analyse_passe_1 (root->opr[0]);
-      analyse_passe_1 (root->opr[1]);
+      // Appels récursifs
+      analyse_passe_1 (root -> opr[0]);
+      analyse_passe_1 (root -> opr[1]);
+
       if(root -> opr[0] -> type == root -> opr[1] -> type)
       {
         // On attibue le type à la NODE_PLUS
@@ -209,8 +246,10 @@ void analyse_global (node_t root)
 
     case NODE_MUL:
 
-      analyse_passe_1 (root->opr[0]);
-      analyse_passe_1 (root->opr[1]);
+      // Appels récursifs
+      analyse_passe_1 (root -> opr[0]);
+      analyse_passe_1 (root -> opr[1]);
+
       if(root -> opr[0] -> type == root -> opr[1] -> type)
       {
         // On attibue le type à la NODE_PLUS
@@ -219,9 +258,11 @@ void analyse_global (node_t root)
       break;
 
     case NODE_DIV:
-    
-      analyse_passe_1 (root->opr[0]);
-      analyse_passe_1 (root->opr[1]);
+
+      // Appels récursifs
+      analyse_passe_1 (root -> opr[0]);
+      analyse_passe_1 (root -> opr[1]);
+
       if(root -> opr[0] -> type == root -> opr[1] -> type)
       {
         // On attibue le type à la NODE_PLUS
@@ -231,14 +272,17 @@ void analyse_global (node_t root)
 
     case NODE_PRINT:
 
-      analyse_passe_1 (root->opr[0]);
+      // Appels récursifs
+      analyse_passe_1 (root -> opr[0]);
       break;
 
     case NODE_STRINGVAL:
 
+      // Opérations sur la NODE_STRINGVAL
       analyse_node_stringval (root);
       break;
 
+    // Pour toutes les autres nodes non définies dans le switch
     default:
       break;
 
@@ -265,7 +309,7 @@ void analyse_passe_1 (node_t root)
       }
       else
       {
-         analyse_passe_1 (root->opr[0]);
+         analyse_passe_1 (root -> opr[0]);
       }
       pop_context ();
       break;
@@ -347,9 +391,9 @@ void analyse_passe_1 (node_t root)
     case NODE_FUNC:
 
       push_context ();
-      analyse_passe_1 (root->opr[0]);
+      analyse_passe_1(root -> opr[0]);
       analyse_passe_1(root -> opr[1]);
-      analyse_passe_1 (root->opr[2]);
+      analyse_passe_1(root -> opr[2]);
       analyse_node_func(root);
       pop_context ();
       break;
@@ -357,8 +401,8 @@ void analyse_passe_1 (node_t root)
     case NODE_BLOCK:
 
       push_context ();
-      analyse_passe_1 (root->opr[0]);
-      analyse_passe_1 (root->opr[1]);
+      analyse_passe_1 (root -> opr[0]);
+      analyse_passe_1 (root -> opr[1]);
       pop_context ();
       break;
 
@@ -366,26 +410,26 @@ void analyse_passe_1 (node_t root)
 
       analyse_passe_1 (root->opr[0]);
       analyse_passe_1 (root->opr[1]);
-      analyse_passe_1 (root->opr[2]);
-      analyse_passe_1 (root->opr[3]);
+      analyse_passe_1 (root -> opr[2]);
+      analyse_passe_1 (root -> opr[3]);
       break;
 
     case NODE_LT:
 
-      analyse_passe_1 (root->opr[0]);
-      analyse_passe_1 (root->opr[1]);
+      analyse_passe_1 (root -> opr[0]);
+      analyse_passe_1 (root -> opr[1]);
       break;
 
     case NODE_AFFECT:
 
-      analyse_passe_1 (root->opr[0]);
-      analyse_passe_1 (root->opr[1]);
+      analyse_passe_1 (root -> opr[0]);
+      analyse_passe_1 (root -> opr[1]);
       break;
 
     case NODE_PLUS:
 
-      analyse_passe_1 (root->opr[0]);
-      analyse_passe_1 (root->opr[1]);
+      analyse_passe_1 (root -> opr[0]);
+      analyse_passe_1 (root -> opr[1]);
       // On vérifie que le l'addition se fait sur deux
       // entités du même type
 
@@ -399,8 +443,8 @@ void analyse_passe_1 (node_t root)
 
     case NODE_MINUS:
 
-      analyse_passe_1 (root->opr[0]);
-      analyse_passe_1 (root->opr[1]);
+      analyse_passe_1 (root -> opr[0]);
+      analyse_passe_1 (root -> opr[1]);
       if(root -> opr[0] -> type == root -> opr[1] -> type)
       {
         // On attibue le type à la NODE_PLUS
@@ -410,8 +454,8 @@ void analyse_passe_1 (node_t root)
 
     case NODE_MUL:
 
-      analyse_passe_1 (root->opr[0]);
-      analyse_passe_1 (root->opr[1]);
+      analyse_passe_1 (root -> opr[0]);
+      analyse_passe_1 (root -> opr[1]);
       if(root -> opr[0] -> type == root -> opr[1] -> type)
       {
         // On attibue le type à la NODE_PLUS
@@ -421,8 +465,8 @@ void analyse_passe_1 (node_t root)
 
     case NODE_DIV:
     
-      analyse_passe_1 (root->opr[0]);
-      analyse_passe_1 (root->opr[1]);
+      analyse_passe_1 (root -> opr[0]);
+      analyse_passe_1 (root -> opr[1]);
       if(root -> opr[0] -> type == root -> opr[1] -> type)
       {
         // On attibue le type à la NODE_PLUS
@@ -430,11 +474,9 @@ void analyse_passe_1 (node_t root)
       }      
       break;
 
-
-
     case NODE_PRINT:
 
-      analyse_passe_1 (root->opr[0]);
+      analyse_passe_1 (root -> opr[0]);
       break;
 
     case NODE_STRINGVAL:
